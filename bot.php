5f225f93d2a4d4b57df3c6cdf1f03955bcc49cea
chutiwatpreprem <chutiwatpreprem@users.noreply.github.com>
2017-04-18 14:01:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,10 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			$messages .= [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -45,4 +49,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo $content;
+echo "OK";
