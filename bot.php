9428c4e96a2543b41f7ef80853a5dc2609280740
chutiwatpreprem <chutiwatpreprem@users.noreply.github.com>
2017-04-18 16:24:11 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -15,6 +15,11 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+
+		// Get replyToken
+			$replyToken = $event['replyToken'];
+
+
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
@@ -33,18 +38,13 @@ if (!is_null($events['events'])) {
 				$text = $event['message']['text'];
 			}
 
-			
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+		
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
-			
-
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -69,6 +69,44 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+
+
+		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
+
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'sticker',
+				'packageId' => "1",
+				'stickerId' => "1"
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
+		}
+
+
 	}
 }
 echo "Success";
