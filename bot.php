f0e8d871062af60b6e8828246793efe3416ceaf3
chutiwatpreprem <chutiwatpreprem@users.noreply.github.com>
2017-04-19 12:13:04 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -36,7 +36,7 @@ if (!is_null($events['events'])) {
 				$text  = "Bathroom mai? Hee Hee";
 			} 
 			else {
-				$text = $event['message']['text'];
+				$text = $event['message']['text'] ." ".$event['source']['userId'];
 			}
 
 		
@@ -147,47 +147,7 @@ if (!is_null($events['events'])) {
 		}
 
 
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			
-
-			if ($event['message']['text'] == "uid") {
-				
-				$text = $event['message']['text']. " ".$event['source']['userId'];
-				
-			}
-
 		
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_PROXY, $proxy);
-			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
 
 
 	}
