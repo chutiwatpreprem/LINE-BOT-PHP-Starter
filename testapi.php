a5441582bcd831256d1dac2c0eeef1d080b4e3ec
chutiwatpreprem <chutiwat.pre@gmail.com>
2017-04-20 10:14:54 +0700

    .
    .

--- testapi.php
+++ testapi.php
@@ -66,21 +66,21 @@
 
 $bot = new BOT_API($channelSecret, $access_token);
 
+
 echo "test";
-	
-// if (!empty($bot->isEvents)) {
-		
-// 	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+if (!empty($bot->isEvents)) {
+		echo "test2";
+	//$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 
-// 	if ($bot->isSuccess()) {
-// 		echo 'Succeeded!';
-// 		exit();
-// 	}
+	// if ($bot->isSuccess()) {
+	// 	echo 'Succeeded!';
+	// 	exit();
+	// }
 
-// 	// Failed
-// 	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-// 	exit();
+	// Failed
+	// echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	// exit();
 
-// }
+}
 
   ?>
