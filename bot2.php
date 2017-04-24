14e7f6072d3f8f855a1205e9bb226b8322c6a120
chutiwatpreprem <chutiwat.pre@gmail.com>
2017-04-24 15:27:25 +0700

    s

--- bot2.php
+++ bot2.php
@@ -2,7 +2,7 @@
 //header("content-type:text/javascript;charset=utf-8"); 
 header('Content-Type: text/html; charset=utf-8');
 echo $thai_data = "สวัสดี";
-echo $data = iconv("tis-620","utf-8",$thai_data);
+echo $data = iconv("ISO-8859-11","utf-8",$thai_data);
 echo "Finish";
 
 
