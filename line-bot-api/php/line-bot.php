a551a7195c508668c9ba97940c58b7a816e1eee5
chutiwatpreprem <chutiwat.pre@gmail.com>
2017-04-25 14:45:58 +0700

    s

--- line-bot-api/php/line-bot.php
+++ line-bot-api/php/line-bot.php
@@ -138,7 +138,7 @@ class BOT_API extends LINEBot {
     }
 
     public function replySticker ($replyToken = null, $stickerId = null, $packageId = null ) {
-        $messageBuilder = new StickerMessageBuilder (2,1);
+        $messageBuilder = new StickerMessageBuilder (1,2);
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
             'replyToken' => $replyToken,
             'messages'   => $messageBuilder->buildMessage(),
