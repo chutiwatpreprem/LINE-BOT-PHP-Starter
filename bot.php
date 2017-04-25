e93643e0d38ae2f9ef20b729253c843025de3e52
chutiwatpreprem <chutiwat.pre@gmail.com>
2017-04-25 11:34:35 +0700

    s

--- bot.php
+++ bot.php
@@ -228,9 +228,9 @@ echo "Test"."<br>";
 
 $bot->sendMessageNew("Uf55b4f528d36c8246795e12f636afa08",'กิ๊ฟ');
 
-if (!empty($bot->isEvents)) {
+//if (!empty($bot->isEvents)) {
 		
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message),json_encode($bot->source));
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->events),json_encode($bot->source));
 
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
@@ -241,7 +241,7 @@ if (!empty($bot->isEvents)) {
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
 
-}	
+//}	
 
 // if ($bot->isEvents) {
 	
